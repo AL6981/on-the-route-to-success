5aa38d9a116ed77edcd5172c3a98c6f03ddbccbd
[AL6981] <amylynnarrington@gmail.com>
2018-01-15 15:44:43 -0500

    user story post a question, complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,26 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
-end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      flash[:success] = 'Question successfully added'
+      redirect_to @question
+    else
+      flash[:errors] = 'Failed to save'
+      render action: 'new'
+    end
+  end
+
+  private
+
+    def question_params
+      params.require(:question).permit(:title, :description)
+    end
+  end
