4336e2f7e12324f523fe3c8896fe302fc8764273
[AL6981] <amylynnarrington@gmail.com>
2018-01-15 14:58:21 -0500

    second user story view a question's details

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,8 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all.sort_by{ |q| q.created_at}
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
 end
